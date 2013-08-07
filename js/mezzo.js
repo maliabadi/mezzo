d7e4f89f9d967ccd0b3d18513a0dfdf127e6b017
Matt Aliabadi <mattmaliabadi@gmail.com>
2013-08-07 04:36:46 -0700

    started javascript interpreter

---
+++ js/mezzo.js
@@ -0,0 +1,84 @@
+// Javascript interpreter for Mezzo scripts
+// Deeply unfinished, untested.
+
+Mezzo = new Object();
+
+Mezzo.alterationGesture = function(obj){
+    for (attribute in obj){
+        this[attribute] = obj[attribute];
+    }
+    this.run = function(){
+
+    };
+    return this;
+}
+
+Mezzo.declarationGesture = function(obj){
+    for (attribute in obj){
+        this[attribute] = obj[attribute];
+    }
+    this.run = function(){
+
+    };
+    return this;
+}
+
+Mezzo.bindingGesture = function(obj){
+    for (attribute in obj){
+        this[attribute] = obj[attribute];
+    }
+    this.run = function(){
+
+    };
+    return this;
+}
+
+Mezzo.comparisonGesture = function(obj){
+    for (attribute in obj){
+        this[attribute] = obj[attribute];
+    }
+    this.run = function(){
+
+    };
+    return this;
+}
+
+Mezzo.flowGesture = function(obj){
+    for (attribute in obj){
+        this[attribute] = obj[attribute];
+    }
+    this.run = function(){
+
+    };
+    return this;
+}
+
+Mezzo.invocationGesture = function(obj){
+    for (attribute in obj){
+        this[attribute] = obj[attribute];
+    }
+    this.run = function(){
+
+    };
+    return this;
+}
+
+Mezzo.iterationGesture = function(obj){
+    for (attribute in obj){
+        this[attribute] = obj[attribute];
+    }
+    this.run = function(){
+
+    };
+    return this;
+}
+
+Mezzo.buildProgram = function(obj){
+    for (attribute in obj){
+        this[attribute] = obj[attribute];
+    }
+    this.run = function(){
+
+    };
+    return this;
+}
