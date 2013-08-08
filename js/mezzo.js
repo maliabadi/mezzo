3e3758a1d043e1f15940549373dde0badf008fc6
Matt Aliabadi <mattmaliabadi@gmail.com>
2013-08-08 04:36:52 -0700

    a little getter and setter fun

--- js/mezzo.js
+++ js/mezzo.js
@@ -10,6 +10,8 @@ function MezzoState(obj){
     for (attribute in obj){
         this.objects[attribute] = obj[attribute];
     }
+
+
 }
 
 MezzoState.prototype.generatePathChain = function(ns){
@@ -75,17 +77,56 @@ MezzoState.prototype.setNameSpace = function(ns, v){
     }
 }
 
-function Mezzo (){
-    this.state = new MezzoState({});
+function Mezzo (objects){
+    if (typeof objects != 'undefined'){
+        this._state = new MezzoState(objects);
+    } else {
+        this._state = new MezzoState({});
+    }
+
+    this.__defineGetter__("state", function(){
+        return this._state.objects;
+    });
+
+    this.__defineSetter__("state", function(obj){
+        if ( obj.constructor.name == "MezzoState" ) {
+            this._state = obj;
+        } else if (obj.constructor.name == "Object") {
+            this._state = new MezzoState(obj);
+        }
+    });
+
+    this.get = function(ns){
+        return this._state.getNameSpace(ns);
+    }
+
+    this.set = function(ns){
+        this._state.setNameSpace(ns);
+    }
 }
 
-Mezzo.prototype.declarationGesture = function(ns){
+Mezzo.prototype.declare = function(ns){
     // just declares the given path namespace
-    this.state.setNameSpace(ns);
+    this._state.setNameSpace(ns);
 }
+
 
 function runTests(){
     m = new Mezzo();
-    m.state.setNameSpace({'foo': {'baz': 'bar'}});
-    console.log(m.state.objects);
+    m.state = {'foo': {'baz': 1}};
+    // should show { foo: { baz: 1 } }
+    console.log(m._state.objects)
+    
+    // should show 1
+    console.log(m.get({'foo': 'baz'}));
+    // should show { foo: { baz: 1 } }
+    console.log(m.state);
+    
+    m = new Mezzo();
+    m.declare({'foo': {'baz': 'bar'}});
+    // should show 'bar'
+    console.log(m.state.foo.baz);
+
 }
+
+runTests()
